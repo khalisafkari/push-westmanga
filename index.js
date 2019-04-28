a48c8e1806442917790ada0ee4e2d83695448e3c
khalis afkari <khalis411@gmail.com>
2019-04-29 01:44:47 +0700

    2

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ token:[] })
+  .write()
 
 app.get('/',(req,res)=>{
     res.send('data')
+})
+
+app.get('/add',(req,res)=>{
+    db.get('token')
+    .push({
+        token:req.query.token
+    })
+    .write();
+    res.send(req.query.token);
 })
 
 app.listen(3000)
